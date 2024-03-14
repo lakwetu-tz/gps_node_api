f667dcf3568efc8411e0e7cd7f1ad5a7a206a1bb
shinobi <kali@kali.shinobi.net>
2024-03-14 13:45:57 +0300

    update

--- src/controllers/vehicleController.ts
+++ src/controllers/vehicleController.ts
@@ -46,21 +46,21 @@ export const getVehicleById = async (req: Request, res: Response) => {
 };
 
 // Get all vehicle based on the userId
-export const getVehicleByUserId = async(req: Request, res: Response) => {
+export const getVehicleByUserId = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
         const user = await Users.findByPk(id)
-        if (!user){
-            return res.status(404).json({ error: "User not found"})
+        if (!user) {
+            return res.status(404).json({ error: "User not found" })
         }
 
-        const vehicle = await Vehicle.findAll({ where: { userId: id}})
-        if (!vehicle){
-            return res.status(403).json({ error: "No vehicle found"})
+        const vehicle = await Vehicle.findAll({ where: { userId: id } })
+        if (!vehicle) {
+            return res.status(403).json({ error: "No vehicle found" })
         }
 
         return res.status(200).json(vehicle)
-    }catch(error){
+    } catch (error) {
         console.error("Error fetching vehicle by ID:", error);
         return res.status(500).json({ error: "Internal server error" });
     }
@@ -69,10 +69,10 @@ export const getVehicleByUserId = async(req: Request, res: Response) => {
 // Create a new vehicle
 export const createVehicle = async (req: Request, res: Response) => {
     const { make, plate, deviceId, password, userId } = req.body;
-    
+
     try {
         // Validate VIN and model
-        if (!password || !make || !deviceId || !plate ) {
+        if (!password || !make || !deviceId || !plate) {
             return res.status(400).json({ error: "missing value required" });
         }
 
@@ -92,16 +92,21 @@ export const createVehicle = async (req: Request, res: Response) => {
             return res.status(403).json({ error: "number plate already exists" });
         }
 
-        const existingDevice = await Devices.findOne({ where: { imei: deviceId } });
-        if (!existingDevice) {
+        const existingImei = await Devices.findOne({ where: { imei: deviceId } });
+        if (!existingImei) {
             return res.status(404).json({ error: "Device not registerd" });
+        }
+
+        const existingDevice = await Vehicle.findOne({ where: { deviceId}})
+        if (existingDevice){
+            return res.status(400).json({ error: "Device is registed and used "})
         }
 
         // Create the new vehicle
         const newVehicle = await Vehicle.create({ userId, make, plate, color: "white", status: "registered", deviceId });
         return res.status(201).json(newVehicle);
     } catch (error: unknown) {
-   
+
         const errorMessage = error instanceof Error ? error.message.split('\n')[0] : 'Internal server error';
         console.error("Error message:", errorMessage);
         return res.json({ error: errorMessage })
@@ -119,7 +124,7 @@ export const updateVehicle = async (req: Request, res: Response) => {
             return res.status(404).json({ error: "Vehicle not found" });
         }
 
-        if (userId !== undefined){
+        if (userId !== undefined) {
             const user = await Users.findByPk(userId);
             if (!user) {
                 return res.status(404).json({ error: "User not found" });
@@ -134,7 +139,7 @@ export const updateVehicle = async (req: Request, res: Response) => {
         }
 
         if (deviceId !== undefined) {
-            const device = await Devices.findOne({ where: { imei: deviceId }});
+            const device = await Devices.findOne({ where: { imei: deviceId } });
             if (!device) {
                 return res.status(404).json({ error: "Device not found" });
             }
@@ -150,7 +155,7 @@ export const updateVehicle = async (req: Request, res: Response) => {
         if (model !== undefined) updatedFields.model = model;
         if (type !== undefined) updatedFields.type = type;
         if (year !== undefined) updatedFields.year = year;
-    
+
 
         if (Object.keys(updatedFields).length === 0) {
             return res.status(400).json({ error: "No valid fields to update" });
--- src/routes/vehicleRoutes.ts
+++ src/routes/vehicleRoutes.ts
@@ -1,6 +1,6 @@
 
 import express from "express";
-import { createVehicle, getVehicleById, updateVehicle, deleteVehicle, getAllVehicles } from "../controllers/vehicleController";
+import { createVehicle, getVehicleById, updateVehicle, deleteVehicle, getAllVehicles, getVehicleByUserId } from "../controllers/vehicleController";
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post("/create", createVehicle);
 
 router.get("/get/:id", getVehicleById);
+
+router.get("/user/:id", getVehicleByUserId);
 
 router.get("/all", getAllVehicles);
 
